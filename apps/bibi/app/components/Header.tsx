a669ea631a858aa47d1c2fcf2d89293ccefa270f
tsubachoko <tsuba.choko@gmail.com>
2025-05-18 21:52:23 +0900

    アイコンの設定

---
+++ apps/bibi/app/components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <div className="flex justify-center">
+      <img
+        src="/icon.png"
+        alt="1+1"
+        style={{
+          objectFit: 'cover',
+          width: '100px',
+          height: '100%',
+        }}
+      />
+    </div>
+  )
+}
--- apps/bibi/app/routes/layout.tsx
+++ apps/bibi/app/routes/layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router'
+import Header from '~/components/Header'
 
 export default function Terms() {
   return (
     <div className="flex justify-center">
       <div className="w-full max-w-4xl p-2">
+        <Header />
         <Outlet />
       </div>
     </div>
